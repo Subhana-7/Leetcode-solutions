a5c849bc884bace440689f534de7c4d25832e0f8
Subhana SN <subhanasn2005@gmail.com>
2025-04-14 20:06:53 +0530

    Time: 63 ms (15.39%), Space: 55.5 MB (29.77%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -6,7 +6,7 @@
 var twoSum = function(nums, target) {
     for(let i = 0; i < nums.length; i++){
         for(let j = 1; j < nums.length; j++){
-            if(nums[i] + nums[j] == target){
+            if(nums[i] + nums[j] == target &&  i !== j){
                 return [i,j];
             }
         }
