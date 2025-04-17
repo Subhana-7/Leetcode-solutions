4709100bf9fe5e223d46cf065df6fe212cf07c53
Subhana SN <subhanasn2005@gmail.com>
2025-04-17 14:10:56 +0530

    Time: 55 ms (8.73%), Space: 56.4 MB (5.02%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+var mergeAlternately = function(word1, word2) {
+    let longestString = Math.max(word1.length,word2.length);
+    let result = "";
+    for(let i = 0; i < longestString; i++){
+        if(word1[i] !== undefined ) result += word1[i]; 
+        if(word2[i] !== undefined ) result += word2[i];
+    }
+    return result;
+};
