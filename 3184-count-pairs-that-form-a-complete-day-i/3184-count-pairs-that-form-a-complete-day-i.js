ec53af89884307671cb9e32b7630d57a9a4037ed
Subhana SN <subhanasn2005@gmail.com>
2025-05-25 15:59:48 +0530

    Time: 0 ms (100%), Space: 57.2 MB (6.52%) - LeetHub

--- 3184-count-pairs-that-form-a-complete-day-i/3184-count-pairs-that-form-a-complete-day-i.js
+++ 3184-count-pairs-that-form-a-complete-day-i/3184-count-pairs-that-form-a-complete-day-i.js
@@ -4,9 +4,9 @@
  */
 var countCompleteDayPairs = function (hours) {
     let count = 0;
-    for (let i = 0; i < hours.length; i++) {
-        for (let j = 0; j < hours.length; j++) {
-            if (i < j && (hours[i] + hours[j]) % 24 == 0) {
+    for (let i = 0; i < hours.length - 1; i++) {
+        for (let j = i + 1; j < hours.length; j++) {
+            if ((hours[i] + hours[j]) % 24 == 0) {
                 count++;
             }
         }
