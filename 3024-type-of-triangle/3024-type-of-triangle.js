2929e935c6e7e2faf9e0a72385012a4e0bab218d
Subhana SN <subhanasn2005@gmail.com>
2025-06-01 17:21:10 +0530

    Time: 0 ms (100%), Space: 55.4 MB (37.23%) - LeetHub

--- 3024-type-of-triangle/3024-type-of-triangle.js
+++ 3024-type-of-triangle/3024-type-of-triangle.js
@@ -4,7 +4,7 @@
  */
 var triangleType = function (nums) {
     if (nums[0] === nums[1] && nums[2] === nums[1]) {
-        return 'equilateral';
+        return 'equilateral' ;
     } else if ((nums[0] + nums[1] <= nums[2]) || (nums[1] + nums[2] <= nums[0]) || (nums[2] + nums[0] <= nums[1])) {
         return 'none';
     } else if (nums[0] == nums[1] || nums[1] == nums[2] || nums[0] == nums[2]) {
