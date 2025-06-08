af3569a72d5e29520fb910ff2fef3452a212201a
Subhana SN <subhanasn2005@gmail.com>
2025-06-09 01:15:09 +0530

    Time: 1 ms (97.62%), Space: 60.3 MB (36%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function (candidates, target) {
+    const result = [];
+    const path = [];
+    backtrack(candidates, 0, target, path, result);
+    return result;
+};
+
+function backtrack(candidates, start, target, path, result) {
+    if (target === 0) {
+        result.push([...path]);
+        return;
+    }
+    if (target < 0) {
+        return;
+    }
+    for (let i = start; i < candidates.length; i++) {
+        path.push(candidates[i]);
+        backtrack(candidates, i, target - candidates[i], path, result);
+        path.pop()
+    }
+}
