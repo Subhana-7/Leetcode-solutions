0f6b9d39f6c0080c36c9fe7fea5aa804d45948ee
Subhana SN <subhanasn2005@gmail.com>
2025-06-25 23:20:57 +0530

    Time: 1 ms (51.19%), Space: 54.9 MB (19.05%) - LeetHub

---
+++ 3280-convert-date-to-binary/3280-convert-date-to-binary.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} date
+ * @return {string}
+ */
+var convertDateToBinary = function (date) {
+    date = date.split('-');
+    date = date.map(item => Number(item).toString(2));
+    return date.join('-');
+};
