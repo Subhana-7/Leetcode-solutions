a1dc95f1a33bb01329121a9224e4f3cb68a08cf2
Subhana SN <subhanasn2005@gmail.com>
2025-07-20 11:19:41 +0530

    Time: 1 ms (69.04%), Space: 55.6 MB (51.88%) - LeetHub

--- 3340-check-balanced-string/3340-check-balanced-string.js
+++ 3340-check-balanced-string/3340-check-balanced-string.js
@@ -2,16 +2,16 @@
  * @param {string} num
  * @return {boolean}
  */
-var isBalanced = function (num) {
-    let a = num.split('')
-    let odd = 0
-    let even = 0
-    for (let i = 0; i < a.length; i++) {
-        if (i % 2 == 0) {
-            even += Number(a[i])
-        } else {
-            odd += Number(a[i])
+var isBalanced = function(num) {
+     let a=num.split('')
+    let odd=0
+    let even=0
+    for(let i=0;i<a.length;i++){
+        if(i%2==0){
+            even+=Number(a[i])
+        }else{
+            odd+=Number(a[i])
         }
     }
-    return odd === even
+    return odd===even;
 };
