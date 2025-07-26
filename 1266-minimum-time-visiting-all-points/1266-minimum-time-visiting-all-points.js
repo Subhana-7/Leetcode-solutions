663653f29355836b013cea62c4e6b1ba31c7e8e2
Subhana SN <subhanasn2005@gmail.com>
2025-07-26 21:54:42 +0530

    Time: 1 ms (76.68%), Space: 54.4 MB (64.57%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.js
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.js
@@ -2,12 +2,12 @@
  * @param {number[][]} points
  * @return {number}
  */
-var minTimeToVisitAllPoints = function(points) {
-    let time=0
-    for(let i=1;i<points.length;i++){
-        let dx=Math.abs(points[i][0]- points[i-1][0])
-        let dy=Math.abs(points[i][1]-points[i-1][1])
-        time+=Math.max(dx,dy)
+var minTimeToVisitAllPoints = function (points) {
+    let time = 0;
+    for (let i = 1; i < points.length; i++) {
+        let dx = Math.abs(points[i][0] - points[i - 1][0]);
+        let dy = Math.abs(points[i][1] - points[i - 1][1]);
+        time += Math.max(dx, dy);
     }
-    return time
+    return time;
 };
