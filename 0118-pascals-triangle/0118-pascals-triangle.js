c07643f88b6dd893264ff127df794920d693736c
Subhana SN <subhanasn2005@gmail.com>
2025-08-01 21:45:39 +0530

    Time: 0 ms (100%), Space: 54 MB (73.96%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function (numRows) {
+    if (numRows == 0) return [];
+    if (numRows == 1) return [[1]];
+
+    let prevRows = generate(numRows - 1);
+    let prevRow = prevRows[prevRows.length - 1];
+    let currentRow = [1];
+
+    for (let i = 1; i < numRows - 1; i++) {
+        currentRow.push(prevRow[i - 1] + prevRow[i]);
+    }
+    currentRow.push(1);
+    prevRows.push(currentRow);
+    return prevRows;
+};
