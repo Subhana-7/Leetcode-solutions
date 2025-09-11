877e5fe0e87532312bb56141abf8613fbb712a96
Subhana SN <subhanasn2005@gmail.com>
2025-09-11 22:35:34 +0530

    Time: 67 ms (84.21%), Space: 70.7 MB (49.47%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.js
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.js
@@ -2,16 +2,16 @@
  * @param {string} s
  * @return {string}
  */
-var sortVowels = function(s) {
+var sortVowels = function (s) {
     const vowels = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']);
     const sArr = s.split('');
     const sortedVowel = sArr.filter((c) => vowels.has(c)).sort();
 
     let result = '', vowelIndex = 0;
-    for(let i = 0; i < s.length; i++){
-        if(vowels.has(s[i])){
+    for (let i = 0; i < s.length; i++) {
+        if (vowels.has(s[i])) {
             result += sortedVowel[vowelIndex++];
-        }else{
+        } else {
             result += s[i];
         }
     }
