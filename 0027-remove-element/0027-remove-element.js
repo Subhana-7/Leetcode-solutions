2eb0654a6e075dc154d2736b604540dd6864fa50
Subhana SN <subhanasn2005@gmail.com>
2025-10-03 22:06:48 +0530

    Time: 0 ms (100%), Space: 54.2 MB (30.45%) - LeetHub

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -5,14 +5,9 @@
  */
 var removeElement = function(nums, val) {
     let count = 0;
-    for(let i = 0; i < nums.length; i++){
-        if(nums[i] === val){
-            nums[i] = '_';
-        }
-    }
     let k = 0;
     for(let i = 0; i < nums.length; i++){
-        if(nums[i] !== '_'){
+        if(nums[i] !== val){
             let temp = nums[k];
             nums[k] = nums[i];
             nums[i] = temp;
