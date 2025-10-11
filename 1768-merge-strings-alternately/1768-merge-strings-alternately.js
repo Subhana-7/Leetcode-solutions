167fe67ba5767faafa02d74532073d53e6174ce6
Subhana SN <subhanasn2005@gmail.com>
2025-10-11 15:34:55 +0530

    Time: 41 ms (78%), Space: 54.3 MB (56.5%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.js
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -3,12 +3,13 @@
  * @param {string} word2
  * @return {string}
  */
-var mergeAlternately = function(word1, word2) {
-    let longestString = Math.max(word1.length,word2.length);
+var mergeAlternately = function (word1, word2) {
+    let longestString;
+    word1.length > word2.length ? longestString = word1.length : longestString = word2.length;
     let result = "";
-    for(let i = 0; i < longestString; i++){
-        if(word1[i] !== undefined ) result += word1[i]; 
-        if(word2[i] !== undefined ) result += word2[i];
+    for (let i = 0; i < longestString; i++) {
+        if (word1[i] !== undefined) result += word1[i];
+        if (word2[i] !== undefined) result += word2[i];
     }
     return result;
 };
