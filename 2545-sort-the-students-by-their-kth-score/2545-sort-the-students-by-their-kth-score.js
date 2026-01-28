3db01207255fbf57da375668b12e13712ecb42a8
Subhana SN <subhanasn2005@gmail.com>
2026-01-28 21:16:34 +0530

    Time: 0 ms (100%), Space: 65.1 MB (23.26%) - LeetHub

--- 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.js
+++ 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.js
@@ -4,12 +4,5 @@
  * @return {number[][]}
  */
 var sortTheStudents = function (score, k) {
-    for (let i = 0; i < score.length; i++) {
-        for (let j = 0; j < score.length; j++) {
-            if (score[i][k] > score[j][k]) {
-                [score[i], score[j]] = [score[j], score[i]];
-            }
-        }
-    }
-    return score;
+    return score.sort((a, b) => b[k] - a[k]);
 };
