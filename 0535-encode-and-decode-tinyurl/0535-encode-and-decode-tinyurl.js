29685cec1b2168316d821cd2657c96a4c2ae9772
Subhana SN <subhanasn2005@gmail.com>
2026-03-25 21:52:05 +0530

    Time: 53 ms (38.2%), Space: 55.7 MB (66.29%) - LeetHub

---
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.js
@@ -0,0 +1,29 @@
+/**
+ * Encodes a URL to a shortened URL.
+ *
+ * @param {string} longUrl
+ * @return {string}
+ */
+
+let hashMap = new Map()
+var encode = function (longUrl) {
+    const code = Math.random().toString(32).slice(5)
+    const tinyUrl = `https://tinyurl.com/${code}`
+    hashMap.set(tinyUrl, longUrl)
+    return tinyUrl
+};
+
+/**
+ * Decodes a shortened URL to its original URL.
+ *
+ * @param {string} shortUrl
+ * @return {string}
+ */
+var decode = function (shortUrl) {
+    return hashMap.get(shortUrl)
+};
+
+/**
+ * Your functions will be called as such:
+ * decode(encode(url));
+ */
