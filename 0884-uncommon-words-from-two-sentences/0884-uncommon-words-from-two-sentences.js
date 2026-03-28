7e69f34e1e13d9b3dee9d955038e9a2be703ba5a
Subhana SN <subhanasn2005@gmail.com>
2026-03-28 23:53:26 +0530

    Time: 1 ms (81.02%), Space: 53.9 MB (65.33%) - LeetHub

--- 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.js
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.js
@@ -3,7 +3,7 @@
  * @param {string} s2
  * @return {string[]}
  */
-var uncommonFromSentences = function(s1, s2) {
-    let word = [...s1.split(' '),...s2.split(' ')];
+var uncommonFromSentences = function (s1, s2) {
+    let word = [...s1.split(' '), ...s2.split(' ')];
     return word.filter(item => word.indexOf(item) === word.lastIndexOf(item));
 };
