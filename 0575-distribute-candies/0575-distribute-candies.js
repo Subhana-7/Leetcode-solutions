0be5c80952118e9968331a66ce852b5d217cd79e
Subhana SN <subhanasn2005@gmail.com>
2026-04-02 23:14:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.js
+++ 0575-distribute-candies/0575-distribute-candies.js
@@ -3,11 +3,7 @@
  * @return {number}
  */
 var distributeCandies = function (candyType) {
-    let freq = {};
-    for (let item of candyType) {
-        freq[item] = (freq[item] | 0) + 1;
-    }
-    let unique = Object.keys(freq).length
-    let halfLen = candyType.length / 2;
-    return Math.min(unique, halfLen)
+    const uniqueTypes = new Set(candyType);
+    const maxCandiesAllowed = candyType.length / 2;
+    return Math.min(uniqueTypes.size, maxCandiesAllowed);
 };
