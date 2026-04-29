219281c9875fdddd1e294d3577f0a1dd2afcf22a
Subhana SN <subhanasn2005@gmail.com>
2026-04-29 22:47:34 +0530

    Time: 3 ms (49.21%), Space: 63.1 MB (46.05%) - LeetHub

---
+++ 3898-find-the-degree-of-each-vertex/3898-find-the-degree-of-each-vertex.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var findDegrees = function (matrix) {
+    let res = [];
+    for (let i = 0; i < matrix.length; i++) {
+        let count = 0;
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (matrix[i][j] === 1) {
+                count++;
+            }
+        }
+        res.push(count);
+    }
+    return res;
+};
