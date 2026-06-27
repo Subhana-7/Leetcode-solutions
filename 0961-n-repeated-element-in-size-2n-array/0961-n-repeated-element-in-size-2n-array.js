71656480deea06db47ced57d284318dde16f8a31
Subhana SN <subhanasn2005@gmail.com>
2026-06-27 22:33:49 +0530

    Time: 0 ms (100%), Space: 56.5 MB (85.45%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.js
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.js
@@ -3,14 +3,11 @@
  * @return {number}
  */
 var repeatedNTimes = function (nums) {
-    let occ = {};
-    for (let num of nums) {
-        occ[num] = (occ[num] || 0) + 1;
-    }
-    let n = (nums.length) / 2;
-    for (let key in occ) {
-        if (occ[key] === n) {
-            return Number(key);
-        }
+    let seen = new Set();
+
+    for (let i of nums) {
+        if (seen.has(i))
+            return i;
+        seen.add(i);
     }
 };
