326154f10c4b638101c3d71c781b6cd15038d8a7
Subhana SN <subhanasn2005@gmail.com>
2026-07-08 23:33:28 +0530

    Time: 2 ms (65.06%), Space: 55.9 MB (60.24%) - LeetHub

---
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[][]} mat
+ * @param {number} k
+ * @return {number[]}
+ */
+var kWeakestRows = function (mat, k) {
+    let rowStrengths = mat.map((row, i) => [row.reduce((a, b) => a + b, 0), i]);
+    rowStrengths.sort((a, b) => a[0] === b[0] ? a[1] - b[1] : a[0] - b[0]);
+    return rowStrengths.slice(0, k).map(([_, i]) => i);
+};
