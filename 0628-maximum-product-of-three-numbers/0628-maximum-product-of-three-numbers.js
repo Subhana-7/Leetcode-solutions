b3c6d3330287580b3bc70fa0d0dc980316ee0fa2
Subhana SN <subhanasn2005@gmail.com>
2026-07-26 22:23:01 +0530

    Time: 38 ms (33.93%), Space: 60.8 MB (5.36%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.js
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.js
@@ -4,5 +4,7 @@
  */
 var maximumProduct = function (nums) {
     nums.sort((a, b) => b - a);
-    return nums[0] * nums[1] * nums[2];
+    let a = nums[0] * nums[1] * nums[2];
+    let b = nums[nums.length - 1] * nums[nums.length - 2] * nums[0];
+    return Math.max(a, b);
 };
