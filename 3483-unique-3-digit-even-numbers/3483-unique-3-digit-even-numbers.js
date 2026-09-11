0bf82746ed5c4cbd1c3617c425794e73b0620fd9
Subhana SN <subhanasn2005@gmail.com>
2026-09-11 23:06:16 +0530

    Time: 6 ms (50%), Space: 57.5 MB (45.24%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.js
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.js
@@ -10,8 +10,8 @@ const totalNumbers = digits => {
         for (let j = 0; j < 10; j++)
             for (let k = 0; k < 9; k += 2)
                 res += (f[i] ?? 0) > 0 &&
-                    (f[j] ?? 0) > (i === j) &&
-                    (f[k] ?? 0) > (i === k) + (j === k);
+                       (f[j] ?? 0) > (i === j) &&
+                       (f[k] ?? 0) > (i === k) + (j === k);
 
     return res;
 };
